df4de0a7f473b071833e21ed6dc01d606cec9b6e
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-12-08 22:50:55 +0200

    Added comment

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ import { useSelector } from "react-redux";
 import { RootState } from "./app/store";
 import Cart from "./components/Card";
 
+/*
+(H)ere we are using the useSelector hook to get the state of the cart from the store.
+(I)f the cart is open, we render the Cart component.
+(R)emember that the Cart component is a modal, so it will be rendered on top of the other components.
+(E)ach component is wrapped in a fragment, so we can return multiple components.
+
+(M)ore info about the useSelector hook: https://react-redux.js.org/api/hooks#useselector
+(e).g. const isOpen = useSelector((state: RootState) => state.cart.isOpen);
+*/
+
 const App = () => {
   const isOpen = useSelector((state: RootState) => state.cart.isOpen);
   return (
