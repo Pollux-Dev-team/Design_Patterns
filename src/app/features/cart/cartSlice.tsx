a693b38bf0ad6e14a1a4b21376157a232a37a35c
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-12-09 10:18:46 +0200

    Finished increment and decrement functionality

--- src/app/features/cart/cartSlice.tsx
+++ src/app/features/cart/cartSlice.tsx
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { ProductType } from "../../../pages/ProductsPage";
 
-// TODO: add types for cart items
 export interface CartItem {
   id: number;
 }
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,11 @@
 import { nLetters } from "../utils/nLetters";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  incrementQuantity,
+  decrementQuantity,
+} from "../app/features/cart/cartSlice";
+import { RootState } from "../app/store";
+
 interface CartItemProps {
   id: number;
   title: string;
@@ -7,19 +14,38 @@ interface CartItemProps {
 }
 
 const CartItem = ({ id, title, price, image }: CartItemProps) => {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state: RootState) => state.cart);
+
+  const itemInCart = cartItems.find((item) => item.id === id);
+
+  const increment = () => {
+    dispatch(incrementQuantity({ id }));
+  };
+
+  const decrement = () => {
+    dispatch(decrementQuantity({ id }));
+  };
+
   return (
     <div className=" flex h-24 justify-between content-end mx-3 mb-6">
       <img src={image} alt="product_image" />
       <div className=" flex flex-col  justify-evenly ">
         <p>{nLetters(title, 15)}</p>
-        <p>{price}</p>
+        <p>{price} EGP</p>
       </div>
       <div className="flex gap-6  self-end mb-3">
-        <div className=" border border-solid border-black  flex h-6 w-5  justify-center items-center font-bold">
+        <div
+          className=" border border-solid border-black  flex h-6 w-5  justify-center items-center font-bold"
+          onClick={decrement}
+        >
           -
         </div>
-        1
-        <div className="border border-solid border-black  flex h-6 w-5  justify-center items-center font-bold">
+        {itemInCart?.quantity}
+        <div
+          className="border border-solid border-black  flex h-6 w-5  justify-center items-center font-bold"
+          onClick={increment}
+        >
           +
         </div>
       </div>
