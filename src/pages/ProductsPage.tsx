96b0e4d5a6b8e34226ade3dbc2c67c4036628769
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-12-28 21:32:16 +0200

    Finalized main page

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -16,34 +16,76 @@ export type ProductType = {
 
 const ProductsPage = () => {
   const getProduct = async () => {
-    const res = await axios.get("https://fakestoreapi.com/products");
-    console.log(res.data);
-    return res.data;
+    const res = await axios.get(
+      "https://book-ordering-system.herokuapp.com/books"
+    );
+
+    return res.data.books;
   };
 
-  const { data, isError, isLoading } = useQuery("products", getProduct);
+  const getProductCategories = async () => {
+    const res = await axios.get(
+      "https://book-ordering-system.herokuapp.com/book_categories"
+    );
+    return res.data.book_categories;
+  };
+
+  // @ts-ignore
+  const {
+    data: productsData,
+    isError: productsIsError,
+    isLoading: productsIsLoading,
+  } = useQuery("products", getProduct);
+
+  const {
+    // @ts-ignore
+    data: productCategoriesData,
+    // @ts-ignore
+    isError: productCategoriesIsError,
+    // @ts-ignore
+    isLoading: productCategoriesIsLoading,
+  } = useQuery("productCategories", getProductCategories);
 
   return (
-    <div>
-      <div className=" mb-16 mt-28 lg:flex lg:flex-wrap  lg:gap-6 w-full lg:justify-center  ">
-        {!isLoading &&
-          !isError &&
-          data?.map((product: ProductType) => (
+    <div className="mt-28">
+      <div className="flex gap-4 flex-wrap justify-center mb-4">
+        {!productCategoriesIsLoading &&
+          !productCategoriesIsError &&
+          productCategoriesData?.map((category: any) => (
+            <div className="  bg-red-600  py-1 px-3 hover:cursor-pointer rounded-md hover:bg-red-700 ">
+              <p className="text-2xl font-bold  text-white">
+                {category.category}
+              </p>
+            </div>
+          ))}
+        {productCategoriesIsLoading && (
+          <div>
+            <LoadindIndicator />
+          </div>
+        )}
+        {productCategoriesIsError && (
+          <p>An error has occured, please try again!</p>
+        )}
+      </div>
+      <div className=" mb-16   lg:flex lg:flex-wrap  lg:gap-6 w-full lg:justify-center  ">
+        {!productsIsLoading &&
+          !productsIsError &&
+          productsData?.map((product: any) => (
             <ProductCard
               key={product.id}
-              productName={product.title!}
+              productName={product.name!}
               productPrice={product.price!}
-              productImage={product.image!}
+              productImage={product.cover_url!}
               productId={product.id!}
             />
           ))}
       </div>
-      {isLoading && (
+      {productsIsLoading && (
         <div className=" flex w-full justify-center  mt-28 ">
           <LoadindIndicator />
         </div>
       )}
-      {isError && <p>An error has occured, please try again!</p>}
+      {productsIsError && <p>An error has occured, please try again!</p>}
     </div>
   );
 };
